2405524efa4575585dbd1f31ff0b8bfc93c1840d
Francois Lambert <francoisobob@gmail.com>
2024-05-20 12:46:32 -0400

    created Xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Francois Lambert on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
